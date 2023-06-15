a051ff7223c76841e9b9632c765e882bb3b86368
Sulaiman Misri <msulaimanmisri@gmail.com>
2023-06-15 13:06:39 +0800

    Initialize

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <title>Guzzle from scatch</title>
+</head>
+
+<body class="bg-light">
+    <div class="container text-center mt-5">
+        <div class="col-12 col-md-6 mx-auto">
+            <div class="greeting">
+                <h1 class="display-4 fw-bold"> Unsepelesh. </h1>
+                <span class="text-muted">
+                    A simple web application for me to refresh my knowledge about Guzzle HTTP.
+                </span>
+            </div>
+
+            <div class="formBody my-5">
+                <form action="" class="form-group">
+                    <input type="text" class="form-control">
+                    <div class="form-text">** Please insert any image keywords that you wanted. E.g Cats</div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
