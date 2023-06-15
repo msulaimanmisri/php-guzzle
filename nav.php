7b4fef0cbf9052b4cac4764790dfb0281cfc2053
Muhamad Sulaiman <msulaimanmisri@gmail.com>
2023-06-15 23:24:53 +0800

    add How to Use

--- nav.php
+++ nav.php
@@ -8,9 +8,28 @@
 
         <div class="collapse navbar-collapse d-md-flex justify-content-end" id="navbarNavAltMarkup">
             <div class="navbar-nav">
-                <a class="nav-link" href="https://github.com/msulaimanmisri" target="_blank">About Me</a>
+                <a class="nav-link" data-bs-toggle="modal" data-bs-target="#exampleModal">How to use</a>
+                <a class="nav-link" href="https://bit.ly/daftar-kelas-design" target="_blank">Learn to code</a>
                 <a class="nav-link" href="https://bit.ly/daftar-kelas-design" target="_blank">Learn to code</a>
             </div>
         </div>
     </div>
 </nav>
+
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+
+            <div class="modal-body">
+                Simply search the image you love. And then after the image is fully loaded, you can right click at it and choose whatever option you want. Such as Open in new tab or copy link.
+
+                <div class="mt-3">
+                    <button type="button" class="btn btn-secondary btn-sm" data-bs-dismiss="modal">Close</button>
+                </div>
+            </div>
+
+
+
+        </div>
+    </div>
+</div>
